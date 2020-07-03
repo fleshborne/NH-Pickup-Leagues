22502d680366c02e03047161c1fff87b55aba8b7
Fleshborne <blakeathompson94@gmail.com>
2020-07-03 14:44:33 -0400

    game model

--- models/game.js
+++ models/game.js
@@ -1,28 +1,39 @@
+/* eslint-disable linebreak-style */
 module.exports = (sequelize, DataTypes) => {
-    const Game = sequelize.define("Game", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1],
-        },
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+  const Game = sequelize.define('Game', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
         len: [1],
+        // eslint-disable-next-line linebreak-style
+      },
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1],
+    },
+    location: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1],
+    },
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: true,
+    // I want updatedAt to actually be called updateTimestamp
+    updatedAt: 'updateTimestamp',
+  });
+  Game.associate = (models) => {
+    // We're saying that a Game should belong to an Author
+    // A Game can't be created without an Author due to the foreign key constraint
+    Game.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
       },
     });
-    Post.associate = function(models) {
-      // We're saying that a Post should belong to an Author
-      // A Post can't be created without an Author due to the foreign key constraint
-      Post.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false,
-        },
-      });
-    };
-  
-    return Post;
   };
-  
+
+  return Game;
+};
