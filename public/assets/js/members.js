1fd0d92b47ad93e5f52cbeec6d285bfbbd759087
Steffi Joel <steffijerome0809>
2020-07-07 19:06:25 -0400

    final changes

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable comma-dangle */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 /* eslint-disable linebreak-style */
 $(document).ready(() => {
@@ -62,49 +64,50 @@ $(document).ready(() => {
 
 // get all the games
 // eslint-disable-next-line no-undef
-// axios.get('/api//user_schedule').then((schedule) => {
-//   // code goes here
-//   console.log(schedule);
-//   schedule.data.forEach((game) => {
-//     console.log(game);
-//     const $table = $('#schedule-table tbody');
-//     const $rowCardTable = $('#rowCardAppend');
-//     let imageCardPath = './assets/images/';
-//     $table.append(`<tr>
-//       <td>${game.gameTypesName}</td>
-//       <td>${game.updatedAt}</td>
-//       <td>Prescott Park</td>
-//       <td>${game.minPlayers}</td>
-//       <td>${game.maxPlayers}</td>
-//     </tr>`);
-//     // try card
-//     if (game.gameTypesName === 'Soccer') {
-//       imageCardPath = `${imageCardPath}soccer.jpg`;
-//     } else if (game.gameTypesName === 'Volleyball') {
-//       imageCardPath = `${imageCardPath}vball.jpg`;
-//       // eslint-disable-next-line no-empty
-//     } else {}
-//     $rowCardTable.append(
-//       ` <div class="col s12 m7">
-//               <div class="card horizontal">
-//                 <div class="card-image">
-//                   <img src="${imageCardPath}">
-//                   <a class="btn-floating halfway-fab waves-effect waves-light red"><i
-//                       class="material-icons">delete</i></a>
-//                   <span class="card-title">${game.gameTypesName}</span>
-//                 </div>
-//                 <div class="card-stacked">
-//                   <div class="card-content grey darken-3 card-font">
-//                     <p>Date: Sunday July 2
-//                     </p>
-//                     <p>Time: ${game.time}
-//                     </p>
-//                     <p>Players: ${game.minPlayers}
-//                     </p>
-//                   </div>
-//                 </div>
-//               </div>
-//               </div>`,
-//     );
-//   });
-// });
+axios.get('/api//user_schedule').then((schedule) => {
+  // code goes here
+  console.log(schedule);
+  schedule.data.forEach((game) => {
+    console.log(game);
+    const $table = $('#schedule-table tbody');
+    const $rowCardTable = $('#rowCardAppend');
+    let imageCardPath = './assets/images/';
+    $table.append(`<tr>
+      <td>${game.gameTypesName}</td>
+      <td>${game.updatedAt}</td>
+      <td>Prescott Park</td>
+      <td>${game.minPlayers}</td>
+      <td>${game.maxPlayers}</td>
+    </tr>`);
+    // try card
+    if (game.gameTypesName === 'Soccer') {
+      imageCardPath = `${imageCardPath}soccer.jpg`;
+    } else if (game.gameTypesName === 'Volleyball') {
+      imageCardPath = `${imageCardPath}vball.jpg`;
+      // eslint-disable-next-line no-empty
+    } else {
+    }
+    $rowCardTable.append(
+      ` <div class="col s12 m7">
+              <div class="card horizontal">
+                <div class="card-image">
+                  <img src="${imageCardPath}">
+                  <a class="btn-floating halfway-fab waves-effect waves-light red"><i
+                      class="material-icons">delete</i></a>
+                  <span class="card-title">${game.gameTypesName}</span>
+                </div>
+                <div class="card-stacked">
+                  <div class="card-content grey darken-3 card-font">
+                    <p>Date: Sunday July 2
+                    </p>
+                    <p>Time: ${game.time}
+                    </p>
+                    <p>Players: ${game.minPlayers}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              </div>`
+    );
+  });
+});
