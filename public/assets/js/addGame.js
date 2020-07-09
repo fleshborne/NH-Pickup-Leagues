eb693c5a72563012c04ab879eaa98070707a9471
Luba Pecheneva <lp5786766@gmail.com>
2020-07-09 19:48:28 -0400

    gametype dropdown is populated

--- public/assets/js/addGame.js
+++ public/assets/js/addGame.js
@@ -9,45 +9,53 @@
 
 // After entering data, it is appended to the page, and after the "Create" button appears.
 
-// On click "Create", the new "game" is added to the table, to your games.
+// function to display messages (could be used for entry validation) 
 function displaySaved() {
-  document.getElementById('display-message').innerHTML =
-    'Game Saved to My Schedule!';
+  document.getElementById('display-message').innerHTML = 'Game Saved to My Schedule!';
   setTimeout(() => {
     document.getElementById('display-message').innerHTML = ' ';
   }, 1000);
 }
 
-// function initiateClickFunc() {
-//   // initialize
-
-//   // update function for demo purposes
-//   $('#myButton').click(() => {});
-// }
-
 $(document).ready(() => {
-  // const initiateClick = $('#initiate-click');
   const newGameForm = $('.add-game');
-  const gameTypeInput = $('#game-type');
-  const locationInput = $('#myDropdown');
+  const gameTypeInput = $('#type-dropdown');
+  const locationInput = $('#location-dropdown');
+  const timeInput = $('time-dropdown');
+  const dateInput = $('date-dropdown');
 
-  $.get('/api/locations').then((data) => {
-
+  // get the Gametype info
+  $.get('/api/gametypes').then((data) => {
     console.log(data);
+    // loop over the names
+    data.forEach((game) => {
+      console.log(game.gameTypesName);
+      // append them as select options
+      const newGame = $('<option>').attr('value', 1).text(game.gameTypesName);
+      gameTypeInput.append(newGame);
+    });
+    gameTypeInput.formSelect();
+  });
 
+  // get the Locations from db
+  $.get('/api/locations').then((data) => {
+    // loop over the titles
     data.forEach((park) => {
       console.log(park.title);
-      const newOpt = $('<option>').attr('value', 1).text(park.title);
-      $('#myDropdown').append(newOpt);
+      // append them as select options
+      const newLoc = $('<option>').attr('value', 1).text(park.title);
+      locationInput.append(newLoc);
     });
-
-    $('#myDropdown').formSelect();
+    locationInput.formSelect();
   });
+
+
   newGameForm.on('submit', (event) => {
     event.preventDefault();
     console.log(gameTypeInput.val());
     console.log(locationInput.val());
-
+    console.log(timeInput.val());
+    console.log(dateInput.val());
     displaySaved();
   });
 });
